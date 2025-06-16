e86a34dbd23861dcaacf0f7247e3eb6e315fd392
Eduardo Berlatto <eduardoberlatto@gmail.com>
2025-06-16 15:27:42 -0300

    implements domain layer

---
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.dio.domain.repository;
+
+import me.dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository //Anotação não necessária, pois o próprio Spring identifica a herança
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
